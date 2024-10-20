29301e83e596d9fe243d274b39c333ce04eabfaa
NafeesTariq <codingzone43@gmail.com>
2024-10-20 20:34:50 +0500

    registeration New Module

--- lib/Views/RegisterScreens/googleSignInSCreen2.dart
+++ lib/Views/RegisterScreens/googleSignInSCreen2.dart
@@ -1,48 +1,48 @@
-import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:flutter/material.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
-class GoogleSignInScreen extends StatefulWidget {
-  final String url;
+// class GoogleSignInScreen extends StatefulWidget {
+//   final String url;
 
-  const GoogleSignInScreen({super.key, required this.url});
+//   const GoogleSignInScreen({super.key, required this.url});
 
-  @override
-  _GoogleSignInScreenState createState() => _GoogleSignInScreenState();
-}
+//   @override
+//   _GoogleSignInScreenState createState() => _GoogleSignInScreenState();
+// }
 
-class _GoogleSignInScreenState extends State<GoogleSignInScreen> {
-  @override
-  void initState() {
-    super.initState();
-    _launchGoogleSignIn();
-  }
+// class _GoogleSignInScreenState extends State<GoogleSignInScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     _launchGoogleSignIn();
+//   }
 
-  void _launchGoogleSignIn() async {
-    final Uri googleSignInUrl = Uri.parse(widget.url);
+//   void _launchGoogleSignIn() async {
+//     final Uri googleSignInUrl = Uri.parse(widget.url);
 
-    if (await canLaunchUrl(googleSignInUrl)) {
-      await launchUrl(
-        googleSignInUrl,
-        mode: LaunchMode
-            .externalApplication, // Ensures it opens in the external browser
-      );
-    } else {
-      throw 'Could not launch $googleSignInUrl';
-    }
-  }
+//     if (await canLaunchUrl(googleSignInUrl)) {
+//       await launchUrl(
+//         googleSignInUrl,
+//         mode: LaunchMode
+//             .externalApplication, // Ensures it opens in the external browser
+//       );
+//     } else {
+//       throw 'Could not launch $googleSignInUrl';
+//     }
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Google Sign-In"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: _launchGoogleSignIn,
-          child: const Text("Open Google Sign-In"),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Google Sign-In"),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: _launchGoogleSignIn,
+//           child: const Text("Open Google Sign-In"),
+//         ),
+//       ),
+//     );
+//   }
+// }
